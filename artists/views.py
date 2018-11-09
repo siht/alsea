97f6d0f448958b7fc64aefe3b16e8c4113eae353
carlos <radix_10339@hotmail.com>
2018-11-08 19:50:53 -0600

    add ViewSet for artists

--- artists/views.py
+++ artists/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ArtistSerializer
+from .models import Artist
+__all__ = (
+    'ArtistViewSet'
+)
 
-# Create your views here.
+
+class ArtistViewSet(viewsets.ModelViewSet):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
